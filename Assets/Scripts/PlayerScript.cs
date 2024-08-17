39faa08f2ce8a273ccfd8b38d7fb312f1ea78ff5
Katty Polyak <kpolyak1@binghamton.edu>
2024-08-17 12:26:21 -0700

    Basic start on player script

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+
+    private bool isWalking;
+    private void Update() {
+        Vector3 inputVector = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.z = +1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.z = -1;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x = +1;
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            inputVector.y = +2;
+        }
+
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, inputVector.y, inputVector.z);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        
+        print(inputVector.x);
+        print(inputVector.y);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+
+
+}
