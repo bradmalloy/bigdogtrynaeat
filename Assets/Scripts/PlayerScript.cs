079bae347e06b320af1b3e4ebcbfd3720fc35032
Katty Polyak <kpolyak1@binghamton.edu>
2024-08-17 12:48:41 -0700

    he jump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
 
     private bool isWalking;
+    private bool isJumping;
     private void Update() {
         Vector3 inputVector = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W)) {
@@ -23,6 +24,7 @@ public class PlayerScript : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.Space)) {
             inputVector.y = +2;
+            isJumping = true;
         }
 
         inputVector = inputVector.normalized;
@@ -31,10 +33,12 @@ public class PlayerScript : MonoBehaviour
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
         isWalking = moveDir != Vector3.zero;
+        if (!isJumping) {
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        }
 
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        
+        isJumping = false;
         print(inputVector.x);
         print(inputVector.y);
     }
@@ -42,6 +46,12 @@ public class PlayerScript : MonoBehaviour
     public bool IsWalking() {
         return isWalking;
     }
+
+    public bool IsJumping() { 
+        return isJumping; 
+    }
+
+
 
 
 }
