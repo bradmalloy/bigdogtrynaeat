96348bfe007d6a483d2e613a1294aa8743a02d13
Leo-DiCara <leo.dicara@grafana.com>
2024-08-17 16:57:45 -0700

    LIL DOG!
    Started game manager.
    
    Instantiates dog prefab (Now with 1000% mo dog)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,10 +8,18 @@ public class GameManager : MonoBehaviour
     // These are set in the Unity editor beforehand
     [SerializeField] private UiManager _uiManager;
     [SerializeField] private InputManager _inputManager;
-    
+
+    [SerializeField] private GameObject lilDogPrefab;
+    [SerializeField] private GameObject dynamicSpawnLoc;
+    [SerializeField] private GameObject wallSpawnLoc;
+    [SerializeField] private GameObject floorSpawnLoc;
+
     // This is serializable only for debug purposes, generated
     // at runtime.
-    [SerializeField] private PlayerScript thePlayer;
+    [SerializeField] private GameObject thePlayer;
+
+    [SerializeField] private int gameScore;
+    [SerializeField] private System.Timers.Timer gameTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -26,14 +34,13 @@ public class GameManager : MonoBehaviour
         SpawnDog();
         // Now that the Player exists, give it to InputManager
         // and have it start up
-        _inputManager.Init(thePlayer);
+        _inputManager.Init(thePlayer.GetComponent<PlayerScript>());
     }
 
     private void SpawnDog()
     {
         print("Creatin that dawg");
-        // replace this with an Instantiate of a prefab
-        thePlayer = FindObjectOfType<PlayerScript>();
+        thePlayer = Instantiate(lilDogPrefab, new Vector3(0, 0, 0), Quaternion.identity, dynamicSpawnLoc.transform);
     }
 
     // Update is called once per frame
